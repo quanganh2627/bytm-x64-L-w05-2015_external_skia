40f944ee7b086adc45896b3c2ac9981c9434b944
jhan16 <junchao.han@intel.com>
2013-08-07 03:45:17 +0800

    [PORT FROM ABT]SSE2 optimization for four skia basic functions
    BZ: 68426
    
    gerrit#79326
    S32_D16_filter_DX_SSE2, S32_opaque_D32_nofilter_DX_SSE
    Just to optimizae picel value recomputation routines when scaling bitmap
    S32_opaque_D32_nofilter_DX_SSE2 is written with assembly.
    The main contributor is Lei L Li in SOTC
    
    S32_opaque_D32_filter_DXDY, S32_alpha_D32_filter_DXDY
    the two functions are further optimized with assembly.
    The optimization is targeted to HTML5 canvas 2D benchmark (CanvasPerf)
    The main contributor is Yaojie Yan in SOTC
    
    gerrit#80730
    Port Fix camera taking picture crash from otc-ics ANDROID-1738,ANDROID-2073
    
    Category: AOSP Improvement
    Domain: AOSP.Optimization-Browser
    Origin: Internal
    Upstream-Candidate: no, key fix
    
    Change-Id: I0c7038a6f1a084bcceec460a8457bede6e44be96
    Author: Lei Li <lei.l.li@intel.com>
    Orig-ABTChange-Id: Ica461a53aad7ff93f0b066241538f3ca469dc3b3
    Signed-off-by: Jin Yang <jin.a.yang@intel.com>

--- Android.mk
+++ Android.mk
@@ -541,7 +541,8 @@ LOCAL_SRC_FILES += \
     src/opts/SkBitmapProcState_opts_SSSE3.cpp \
     src/opts/SkBlitRect_opts_SSE2.cpp \
     src/opts/opts_check_SSE2.cpp \
-    src/opts/SkUtils_opts_SSE2.cpp
+    src/opts/SkUtils_opts_SSE2.cpp \
+    src/opts/SkBitmapProcState_opts_SSE2_asm.S
 
 else
 LOCAL_SRC_FILES += \
